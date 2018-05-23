489cf207b9395017658d4656142a2490422b7a65
Neha Sardana <neha.sardana@bnymellon.com>
2018-05-23 17:13:40 -0400

    Between two sets

---
+++ src/main/java/com/hackerank/impl/betweentwosets/Solution.java
@@ -0,0 +1,58 @@
+package com.hackerank.impl.betweentwosets;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Solution {
+
+    /*
+     * Complete the getTotalX function below.
+     */
+    static int getTotalX(int[] a, int[] b) {
+        /*
+         * Write your code here.
+         */
+
+    	return 0;
+    }
+
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nm = scan.nextLine().split(" ");
+
+        int n = Integer.parseInt(nm[0].trim());
+
+        int m = Integer.parseInt(nm[1].trim());
+
+        int[] a = new int[n];
+
+        String[] aItems = scan.nextLine().split(" ");
+
+        for (int aItr = 0; aItr < n; aItr++) {
+            int aItem = Integer.parseInt(aItems[aItr].trim());
+            a[aItr] = aItem;
+        }
+
+        int[] b = new int[m];
+
+        String[] bItems = scan.nextLine().split(" ");
+
+        for (int bItr = 0; bItr < m; bItr++) {
+            int bItem = Integer.parseInt(bItems[bItr].trim());
+            b[bItr] = bItem;
+        }
+
+        int total = getTotalX(a, b);
+
+        bw.write(String.valueOf(total));
+        bw.newLine();
+
+        bw.close();
+    }
+}
+
